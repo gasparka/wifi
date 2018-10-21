878fe740d1a9232f9222193a8e809dcfdb3511e6
Gaspar Karm <gkarm@live.com>
2018-10-21 19:50:21 +0300

    add equalizer
    Former-commit-id: 4597ea3cd88663538de50b4298598956a593a739

---
+++ ieee80211phy/receiver/equalizer.py
@@ -0,0 +1,33 @@
+from ieee80211phy.transmitter.preamble import long_train_symbol
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Equalizer:
+    def __init__(self, sample_advance):
+        self.sample_advance = sample_advance
+        self.equalizer_coefs = None
+
+    def train(self, rx, start_of_training_symbol, debug=False):
+        ideal_long = long_train_symbol()
+        first_train = rx[start_of_training_symbol + self.sample_advance-64:start_of_training_symbol + self.sample_advance]
+        second_train = rx[start_of_training_symbol + self.sample_advance : start_of_training_symbol + self.sample_advance + 64]
+        avg_train = (first_train + second_train) / 2
+
+        channel_estimate = np.fft.fft(avg_train) / np.fft.fft(ideal_long)
+        self.equalizer_coefs = 1 / channel_estimate
+
+        if debug:
+            plt.figure(figsize=(9.75, 5))
+            plt.plot(avg_train)
+            plt.plot(first_train)
+            plt.plot(second_train)
+            plt.plot(ideal_long)
+            plt.tight_layout()
+            plt.grid()
+
+            plt.figure(figsize=(9.75, 5))
+            plt.ylim([-60, 20])
+            plt.plot(np.fft.fftshift(20 * np.log10(np.abs(self.equalizer_coefs))))
+            plt.tight_layout()
+            plt.grid()
